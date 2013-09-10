60f8fc24646285b54cf9165940f2fb54aef260a8
adam <adam@localhost>
2013-09-10 12:02:27 +0200

    first

--- bullet-heck/src/org/frej/bulletheck/BulletHeck.java
+++ bullet-heck/src/org/frej/bulletheck/BulletHeck.java
@@ -17,7 +17,8 @@ public class BulletHeck implements ApplicationListener {
 	private Sprite sprite;
 	
 	@Override
-	public void create() {		
+	public void create() {	
+		//
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
